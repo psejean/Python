ad3cbdef2265cd5f2d9360adb9ad2eb32aa1308a
psejean <150192056+psejean@users.noreply.github.com>
2024-04-29 14:41:49 -0400

    Update function.py

--- function.py
+++ function.py
@@ -31,3 +31,4 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )  
+  
